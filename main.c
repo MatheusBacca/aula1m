ad658efc75877b2bc6eb14e1b1c712ea60a0acc2
MatheusBacca <67923747+MatheusBacca@users.noreply.github.com>
2021-08-03 11:01:53 -0300

    mudança: Bacca

--- main.c
+++ main.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 #include <locale.h>
 
-struct funcionario
+typedef struct 
 {
-    char nome;
+    char nome[100];
     int vale;
     int planoSaude;
     float salario;
     int dependentes;
-};
-
+} funcionario;
 
 void main()
 {
-
+	funcionario *func;
+	func = (funcionario*) malloc (sizeof (funcionario));
+	printf("\n\tOlá, seja bem-vindo a folha de pagamento!\n");
+	printf("\n\tEntre com seu nome: ");
 }
